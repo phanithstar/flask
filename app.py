a4e60ae077c1989c658e452f35a802fbbe94f06b
phanithstar <phanithstar2015@gmail.com>
2020-07-15 09:00:08 +0000

    Flask V1.0

---
+++ app.py
@@ -0,0 +1,33 @@
+import time
+
+import redis
+from flask import Flask
+from os import environ
+
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+app.config['FLASK_ENV'] = environ.get('FLASK_ENV')
+mode = app.config['FLASK_ENV']
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello from Docker Fan! I have been seen {} times.\n'.format(count)
+
+@app.route('/name/<name>')
+def get_name(name):
+    return "Hello ," + name + " from " + mode + "\n"
